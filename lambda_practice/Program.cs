874f72e5d24ed1fcd2272bf93052c5eb01c11444
Unknown <rixtrevizo@gmail.com>
2018-03-05 20:25:23 -0700

    homework

--- lambda_practice/Program.cs
+++ lambda_practice/Program.cs
@@ -1,6 +1,7 @@
 ﻿using lambda_practice.data;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 
 namespace lambda_practice
 {
@@ -22,23 +23,41 @@ namespace lambda_practice
             using (var context = new DatabaseContext(options))
             {
                 //You can erase the following 3 lines if needed
-                context.Cities.ForEachAsync(c => Console.WriteLine(c.Name));
-                context.Departments.ForEachAsync(d => Console.WriteLine(d.Name));
-                context.Employees.ForEachAsync(e => Console.WriteLine($"{e.FirstName} {e.LastName}"));
+                //context.Cities.ForEachAsync(c => Console.WriteLine($"{c.Id}, {c.Name}"));
+                //context.Departments.ForEachAsync(d => Console.WriteLine(d.Name));
+                //context.Employees.ForEachAsync(e => Console.WriteLine($"{e.FirstName} {e.LastName}"));
 
                 //1. List all employees whose departament has an office in Chihuahua
+                context.Employees
+                    .Where(e => e.Department.Cities.Any(c => c.Name == "Chihuahua"))
+                    .ForEachAsync(e => Console.WriteLine($"{e.FirstName} {e.LastName}"));
 
 
-                //2. List all departaments and the number of employees that belong to each department.
+
+                //2. List all departments and the number of employees that belong to each department.
+                context.Departments
+                    .ForEachAsync(d => Console.WriteLine($"{d.Name} {context.Employees.Where(e => e.DepartmentId == d.Id).Count()} "));
 
 
                 //3. List all remote employees. That is all employees whose living city is not the same one as their department's.
+                context.Employees
+                    .Where(e => !(e.Department.Cities.Any(c => c.Name == e.City.Name)))
+                    .ForEachAsync(e => Console.WriteLine($"{e.FirstName} {e.LastName}"));
 
 
                 //4. List all employees whose hire aniversary is next month.
-
+                context.Employees
+                    .Where(e => e.HireDate.Month == DateTime.Today.AddMonths(1).Month)
+                    .ForEachAsync(e => Console.WriteLine($"{e.FirstName} {e.LastName}"));
 
                 //5. List all 12 months of the year and the number of employees hired on each month.
+                string[] names = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.MonthNames;
+                for(int i = 0; i < 12; i++)
+                {
+                    Console.WriteLine($"{names[i]} {context.Employees.Where(e => e.HireDate.Month == i + 1).Count()}");
+                    
+                }
+                
             }
 
             Console.ReadLine();
